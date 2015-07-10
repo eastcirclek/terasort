ed2693f87807c2aa82b4b4597335e5d789ddff54
eastcirclek <eastcirclek@gmail.com>
2015-07-10 11:16:21 +0900

    modify FlinkTeraSort and add OptimizedText

--- src/main/java/eastcircle/terasort/TotalOrderPartitioner.java
+++ src/main/java/eastcircle/terasort/TotalOrderPartitioner.java
@@ -53,195 +53,191 @@ import org.apache.hadoop.mapreduce.MRJobConfig;
  */
 public class TotalOrderPartitioner implements Serializable{
 
-	private Configuration conf;
-	private TrieNode trie;
+    private Configuration conf;
+    private TrieNode trie;
 
-	private Path partFile; // eastcirclek
+    private Path partFile; // eastcirclek
 	
-	/**
-	 * A generic trie node
-	 */
-	static abstract class TrieNode {
-		private int level;
-		TrieNode(int level) {
-			this.level = level;
-		}
-		abstract int findPartition(Text key);
-		abstract void print(PrintStream strm) throws IOException;
-		int getLevel() {
-			return level;
-		}
-	}
+    /**
+     * A generic trie node
+     */
+    static abstract class TrieNode {
+        private int level;
+        TrieNode(int level) {
+            this.level = level;
+        }
+        abstract int findPartition(Text key);
+        abstract void print(PrintStream strm) throws IOException;
+        int getLevel() {
+            return level;
+        }
+    }
 
-	/**
-	 * An inner trie node that contains 256 children based on the next
-	 * character.
-	 */
-	static class InnerTrieNode extends TrieNode {
-		private TrieNode[] child = new TrieNode[256];
+    /**
+     * An inner trie node that contains 256 children based on the next
+     * character.
+     */
+    static class InnerTrieNode extends TrieNode {
+        private TrieNode[] child = new TrieNode[256];
       
-		InnerTrieNode(int level) {
-			super(level);
-		}
-		int findPartition(Text key) {
-			int level = getLevel();
-			if (key.getLength() <= level) {
-				return child[0].findPartition(key);
-			}
-			return child[key.getBytes()[level] & 0xff].findPartition(key);
-		}
-		void setChild(int idx, TrieNode child) {
-			this.child[idx] = child;
-		}
-		void print(PrintStream strm) throws IOException {
-			for(int ch=0; ch < 256; ++ch) {
-				for(int i = 0; i < 2*getLevel(); ++i) {
-					strm.print(' ');
-				}
-				strm.print(ch);
-				strm.println(" ->");
-				if (child[ch] != null) {
-					child[ch].print(strm);
-				}
-			}
-		}
-	}
+        InnerTrieNode(int level) {
+            super(level);
+        }
+        int findPartition(Text key) {
+            int level = getLevel();
+            if (key.getLength() <= level) {
+                return child[0].findPartition(key);
+            }
+            return child[key.getBytes()[level] & 0xff].findPartition(key);
+        }
+        void setChild(int idx, TrieNode child) {
+            this.child[idx] = child;
+        }
+        void print(PrintStream strm) throws IOException {
+            for(int ch=0; ch < 256; ++ch) {
+                for(int i = 0; i < 2*getLevel(); ++i) {
+                    strm.print(' ');
+                }
+                strm.print(ch);
+                strm.println(" ->");
+                if (child[ch] != null) {
+                    child[ch].print(strm);
+                }
+            }
+        }
+    }
 
-	/**
-	 * A leaf trie node that does string compares to figure out where the given
-	 * key belongs between lower..upper.
-	 */
-	static class LeafTrieNode extends TrieNode {
-		int lower;
-		int upper;
-		Text[] splitPoints;
-		LeafTrieNode(int level, Text[] splitPoints, int lower, int upper) {
-			super(level);
-			this.splitPoints = splitPoints;
-			this.lower = lower;
-			this.upper = upper;
-		}
-		int findPartition(Text key) {
-			for(int i=lower; i<upper; ++i) {
-				if (splitPoints[i].compareTo(key) >= 0) {
-					return i;
-				}
-			}
-			return upper;
-		}
-		void print(PrintStream strm) throws IOException {
-			for(int i = 0; i < 2*getLevel(); ++i) {
-				strm.print(' ');
-			}
-			strm.print(lower);
-			strm.print(", ");
-			strm.println(upper);
-		}
-	}
+    /**
+     * A leaf trie node that does string compares to figure out where the given
+     * key belongs between lower..upper.
+     */
+    static class LeafTrieNode extends TrieNode {
+        int lower;
+        int upper;
+        Text[] splitPoints;
+        LeafTrieNode(int level, Text[] splitPoints, int lower, int upper) {
+            super(level);
+            this.splitPoints = splitPoints;
+            this.lower = lower;
+            this.upper = upper;
+        }
+        int findPartition(Text key) {
+            for(int i=lower; i<upper; ++i) {
+                if (splitPoints[i].compareTo(key) >= 0) {
+                    return i;
+                }
+            }
+            return upper;
+        }
+        void print(PrintStream strm) throws IOException {
+            for(int i = 0; i < 2*getLevel(); ++i) {
+                strm.print(' ');
+            }
+            strm.print(lower);
+            strm.print(", ");
+            strm.println(upper);
+        }
+    }
 
 
-	/**
-	 * Read the cut points from the given sequence file.
-	 * @param fs the file system
-	 * @param p the path to read
-	 * @param job the job config
-	 * @return the strings to split the partitions on
-	 * @throws IOException
-	 */
-	private static Text[] readPartitions(FileSystem fs, Path p,
-																			 Configuration conf) throws IOException {
-		int reduces = conf.getInt(MRJobConfig.NUM_REDUCES, 1);
-		Text[] result = new Text[reduces - 1];
-		DataInputStream reader = fs.open(p);
-		for(int i=0; i < reduces - 1; ++i) {
-			result[i] = new Text();
-			result[i].readFields(reader);
-		}
-		reader.close();
-		return result;
-	}
+    /**
+     * Read the cut points from the given sequence file.
+     * @param fs the file system
+     * @param p the path to read
+     * @param job the job config
+     * @return the strings to split the partitions on
+     * @throws IOException
+     */
+    private static Text[] readPartitions(FileSystem fs, Path p, Configuration conf) throws IOException {
+        int reduces = conf.getInt(MRJobConfig.NUM_REDUCES, 1);
+        Text[] result = new Text[reduces - 1];
+        DataInputStream reader = fs.open(p);
+        for(int i=0; i < reduces - 1; ++i) {
+            result[i] = new Text();
+            result[i].readFields(reader);
+        }
+        reader.close();
+        return result;
+    }
 
-	/**
-	 * Given a sorted set of cut points, build a trie that will find the correct
-	 * partition quickly.
-	 * @param splits the list of cut points
-	 * @param lower the lower bound of partitions 0..numPartitions-1
-	 * @param upper the upper bound of partitions 0..numPartitions-1
-	 * @param prefix the prefix that we have already checked against
-	 * @param maxDepth the maximum depth we will build a trie for
-	 * @return the trie node that will divide the splits correctly
-	 */
-	private static TrieNode buildTrie(Text[] splits, int lower, int upper, 
-																		Text prefix, int maxDepth) {
-		int depth = prefix.getLength();
-		if (depth >= maxDepth || lower == upper) {
-			return new LeafTrieNode(depth, splits, lower, upper);
-		}
-		InnerTrieNode result = new InnerTrieNode(depth);
-		Text trial = new Text(prefix);
-		// append an extra byte on to the prefix
-		trial.append(new byte[1], 0, 1);
-		int currentBound = lower;
-		for(int ch = 0; ch < 255; ++ch) {
-			trial.getBytes()[depth] = (byte) (ch + 1);
-			lower = currentBound;
-			while (currentBound < upper) {
-				if (splits[currentBound].compareTo(trial) >= 0) {
-					break;
-				}
-				currentBound += 1;
-			}
-			trial.getBytes()[depth] = (byte) ch;
-			result.child[ch] = buildTrie(splits, lower, currentBound, trial, 
-																	 maxDepth);
-		}
-		// pick up the rest
-		trial.getBytes()[depth] = (byte) 255;
-		result.child[255] = buildTrie(splits, currentBound, upper, trial,
-																	maxDepth);
-		return result;
-	}
+    /**
+     * Given a sorted set of cut points, build a trie that will find the correct
+     * partition quickly.
+     * @param splits the list of cut points
+     * @param lower the lower bound of partitions 0..numPartitions-1
+     * @param upper the upper bound of partitions 0..numPartitions-1
+     * @param prefix the prefix that we have already checked against
+     * @param maxDepth the maximum depth we will build a trie for
+     * @return the trie node that will divide the splits correctly
+     */
+    private static TrieNode buildTrie(Text[] splits, int lower, int upper, Text prefix, int maxDepth) {
+        int depth = prefix.getLength();
+        if (depth >= maxDepth || lower == upper) {
+            return new LeafTrieNode(depth, splits, lower, upper);
+        }
+        InnerTrieNode result = new InnerTrieNode(depth);
+        Text trial = new Text(prefix);
+        // append an extra byte on to the prefix
+        trial.append(new byte[1], 0, 1);
+        int currentBound = lower;
+        for(int ch = 0; ch < 255; ++ch) {
+            trial.getBytes()[depth] = (byte) (ch + 1);
+            lower = currentBound;
+            while (currentBound < upper) {
+                if (splits[currentBound].compareTo(trial) >= 0) {
+                    break;
+                }
+                currentBound += 1;
+            }
+            trial.getBytes()[depth] = (byte) ch;
+            result.child[ch] = buildTrie(splits, lower, currentBound, trial, 
+                                         maxDepth);
+        }
+        // pick up the rest
+        trial.getBytes()[depth] = (byte) 255;
+        result.child[255] = buildTrie(splits, currentBound, upper, trial,
+                                      maxDepth);
+        return result;
+    }
 
-	// eastcirclek
-	private static
-		TrieNode buildTrieFromHDFS(Configuration conf,
-															 Path hdfsPath) throws IOException{
-		FileSystem fs = hdfsPath.getFileSystem(conf);
-		Text[] splitPoints = readPartitions(fs, hdfsPath, conf);
-		return buildTrie(splitPoints, 0, splitPoints.length, new Text(), 2);
-	}
+    // eastcirclek
+    private static TrieNode buildTrieFromHDFS(Configuration conf, Path hdfsPath) throws IOException{
+        FileSystem fs = hdfsPath.getFileSystem(conf);
+        Text[] splitPoints = readPartitions(fs, hdfsPath, conf);
+        return buildTrie(splitPoints, 0, splitPoints.length, new Text(), 2);
+    }
 
-	// eastcirclek
-	public TotalOrderPartitioner(Configuration conf,
-															 Path partFile) throws IOException{
-		this.conf = conf;
-		this.partFile = partFile;
-		this.trie = buildTrieFromHDFS(conf, partFile);
-	}
+    // eastcirclek
+    public TotalOrderPartitioner(Configuration conf,
+                                 Path partFile) throws IOException{
+        this.conf = conf;
+        this.partFile = partFile;
+        this.trie = buildTrieFromHDFS(conf, partFile);
+    }
 
-	// eastcirclek
-	public int getPartition(Text key){
-		return trie.findPartition(key);
-	}
+    // eastcirclek
+    public int getPartition(Text key){
+        return trie.findPartition(key);
+    }
 
-	// eastcirclek for serialization
-	private void writeObject(ObjectOutputStream out) throws IOException{
-		out.writeUTF(conf.get("fs.defaultFS"));
-		out.writeInt(conf.getInt(MRJobConfig.NUM_REDUCES, 2));
-		/**
-		 * Instead of serializing the trie,
-		 *  we serialize the filename containing sampling points
-		 *  so that we can rebuild the trie in each task.
-		 */
-		out.writeUTF(this.partFile.toString());
-	}
+    // eastcirclek for serialization
+    private void writeObject(ObjectOutputStream out) throws IOException{
+        out.writeUTF(conf.get("fs.defaultFS"));
+        out.writeInt(conf.getInt(MRJobConfig.NUM_REDUCES, 2));
+        /**
+         * Instead of serializing the trie,
+         *  we serialize the filename containing sampling points
+         *  so that we can rebuild the trie in each task.
+         */
+        out.writeUTF(this.partFile.toString());
+    }
 
-	// eastcirclek for deserialization
-	private void readObject(ObjectInputStream in) throws IOException{
-		this.conf = new Configuration();
-		conf.set("fs.defaultFS", (String)in.readUTF());
-		conf.setInt(MRJobConfig.NUM_REDUCES, (int)in.readInt());
-		this.partFile = new Path((String)in.readUTF());
-		this.trie = buildTrieFromHDFS(conf, partFile);
-	}
+    // eastcirclek for deserialization
+    private void readObject(ObjectInputStream in) throws IOException{
+        this.conf = new Configuration();
+        conf.set("fs.defaultFS", (String)in.readUTF());
+        conf.setInt(MRJobConfig.NUM_REDUCES, (int)in.readInt());
+        this.partFile = new Path((String)in.readUTF());
+        this.trie = buildTrieFromHDFS(conf, partFile);
+    }
 }
--- src/main/scala/eastcircle/terasort/FlinkTeraSort.scala
+++ src/main/scala/eastcircle/terasort/FlinkTeraSort.scala
@@ -13,11 +13,20 @@ import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapred.JobConf
 import org.apache.hadoop.mapreduce.Job
 
+import scala.util.Try
+
 class FlinkTeraPartitioner(underlying:TotalOrderPartitioner) extends Partitioner[Text] {
   def partition(key:Text, numPartitions:Int):Int = {
     underlying.getPartition(key)
   }
 }
+
+class OptimizedFlinkTeraPartitioner(underlying:TotalOrderPartitioner) extends Partitioner[OptimizedText] {
+  def partition(key:OptimizedText, numPartitions:Int):Int = {
+    underlying.getPartition(key.getText())
+  }
+}
+
 
 object FlinkTeraSort {
 
@@ -26,13 +35,27 @@ object FlinkTeraSort {
   }
 
   def main(args: Array[String]){
+    if(args.size != 6){
+      println("Usage: FlinkTeraSort hdfs inputPath outputPath #partitions " +
+              "useOptimizedText(true or false) " +
+              "useObjectReuse(true or false)") 
+      return
+    }
+
     val env = ExecutionEnvironment.getExecutionEnvironment
 
     val hdfs = args(0)
     val inputPath= hdfs+args(1)
     val outputPath = hdfs+args(2)
     val partitions = args(3).toInt
-    
+    val useOptimizedText:Boolean = Try(args(4).toBoolean).getOrElse(true)
+    val useObjectReuse:Boolean = Try(args(5).toBoolean).getOrElse(true)
+    println("useOptimizedText : " + useOptimizedText);
+    println("useObjectReuse : " + true)
+    if(useObjectReuse){
+      env.getConfig.enableObjectReuse()
+    }
+
     val mapredConf = new JobConf()
     mapredConf.set("fs.defaultFS", hdfs)
     mapredConf.set("mapreduce.input.fileinputformat.inputdir", inputPath)
@@ -43,14 +66,26 @@ object FlinkTeraSort {
 
     val partitionFile = new Path(outputPath, TeraInputFormat.PARTITION_FILENAME)
     TeraInputFormat.writePartitionFile(jobContext, partitionFile)
-    val partitioner = new FlinkTeraPartitioner(new TotalOrderPartitioner(mapredConf, partitionFile))
-    
+    val underlyingPartitioner = new TotalOrderPartitioner(mapredConf, partitionFile)
     val teraInputFormat = new TeraInputFormat()
     val teraOutputFormat = new TeraOutputFormat()
     val hadoopOF = new HadoopOutputFormat[Text, Text](teraOutputFormat, jobContext);
 
-    val inputFile = env.readHadoopFile(teraInputFormat, classOf[Text], classOf[Text], inputPath)
-    inputFile.partitionCustom(partitioner, 0).sortPartition(0, Order.ASCENDING).output(hadoopOF)
-    env.execute("TeraSort")
+    if(useOptimizedText){
+      val partitioner = new OptimizedFlinkTeraPartitioner(underlyingPartitioner)
+
+      val inputFile = env.readHadoopFile(teraInputFormat, classOf[Text], classOf[Text], inputPath)
+      val optimizedText = inputFile.map(tp => (new OptimizedText(tp._1), tp._2))
+      val sortedPartitioned = optimizedText.partitionCustom(partitioner, 0).sortPartition(0, Order.ASCENDING)
+      sortedPartitioned.map(tp => (tp._1.getText, tp._2)).output(hadoopOF)
+      env.execute("TeraSort")
+    }else{
+      val partitioner = new FlinkTeraPartitioner(underlyingPartitioner)
+      
+      val inputFile = env.readHadoopFile(teraInputFormat, classOf[Text], classOf[Text], inputPath)
+      val sortedPartitioned = inputFile.partitionCustom(partitioner, 0).sortPartition(0, Order.ASCENDING)
+      sortedPartitioned.output(hadoopOF)
+      env.execute("TeraSort")
+    }
   }
 }
